658ed7b8955ec320de2ae555d53d3735bbbdd992
Daniel Tinivella <dtinivella@gmail.com>
2024-01-11 23:44:51 -0300

    fix added module for authenticable

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -11,9 +11,20 @@ module Authenticable
     @current_user ||= User.find_by(token: request.headers['Authorization'])
   end
 
-  def authenticate_with_token
+  def authenticate_with_token!
     return if current_user.blank?
 
-    render json: { errors: 'Not authorized' }, status: :forbidden
+    render json: { errors: 'Not authenticated' },
+           status: :forbidden
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    begin
+      @decoded = JsonWebToken.decode(header)
+      @current_user = User.find(@decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
   end
 end
